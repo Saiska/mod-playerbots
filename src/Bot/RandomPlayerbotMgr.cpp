d403c501e14d93521b20deece5f5874ab8e91d98
Saiska <nicolas.anderlini@gmail.com>
2026-06-02 02:45:50 +0200

    feat(pastime): log Pastime in rpg-status summary

--- src/Bot/RandomPlayerbotMgr.cpp
+++ src/Bot/RandomPlayerbotMgr.cpp
@@ -2920,11 +2920,11 @@ void RandomPlayerbotMgr::PrintStats()
         LOG_INFO("playerbots", "Bots rpg status:");
         LOG_INFO("playerbots",
                  "    Idle: {}, Rest: {}, GoGrind: {}, GoCamp: {}, MoveRandom: {}, MoveNpc: {}, CityLife: {}, "
-                 "DoQuest: {}, TravelFlight: {}, OutdoorPvP: {}",
+                 "Pastime: {}, DoQuest: {}, TravelFlight: {}, OutdoorPvP: {}",
                  rpgStatusCount[RPG_IDLE], rpgStatusCount[RPG_REST], rpgStatusCount[RPG_GO_GRIND],
                  rpgStatusCount[RPG_GO_CAMP], rpgStatusCount[RPG_WANDER_RANDOM], rpgStatusCount[RPG_WANDER_NPC],
-                 rpgStatusCount[RPG_CITY_LIFE], rpgStatusCount[RPG_DO_QUEST], rpgStatusCount[RPG_TRAVEL_FLIGHT],
-                 rpgStatusCount[RPG_OUTDOOR_PVP]);
+                 rpgStatusCount[RPG_CITY_LIFE], rpgStatusCount[RPG_PASTIME], rpgStatusCount[RPG_DO_QUEST],
+                 rpgStatusCount[RPG_TRAVEL_FLIGHT], rpgStatusCount[RPG_OUTDOOR_PVP]);
 
         LOG_INFO("playerbots", "Bots total quests:");
         LOG_INFO("playerbots", "    Accepted: {}, Rewarded: {}, Dropped: {}", rpgStasticTotal.questAccepted,
